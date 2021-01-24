71d7a422682d41232e04fad8499224461d405846
Aditya <aditya@Adityas-MacBook-Air.local>
2021-01-24 17:41:04 +0530

    added a page for tech realted news

--- my_app/views.py
+++ my_app/views.py
@@ -4,7 +4,7 @@ from newsapi.newsapi_client import NewsApiClient
 
 def index(request):
     newsapi = NewsApiClient(api_key = "a0595ccff26a4790a9dd4b8c1e7e7c01")
-    top = newsapi.get_top_headlines(sources ='techcrunch') 
+    top = newsapi.get_top_headlines(sources ='bbc-news') 
     l = top["articles"]
     desc =[] 
     news =[] 
@@ -17,3 +17,20 @@ def index(request):
         img.append(f['urlToImage']) 
     mylist = zip(news, desc, img) 
     return render(request, 'index.html', context ={"mylist":mylist})
+
+def technews(request):
+    newsapi = NewsApiClient(api_key = "a0595ccff26a4790a9dd4b8c1e7e7c01")
+    top_headlines = newsapi.get_top_headlines(sources = "techcrunch")
+    l = top_headlines["articles"]
+    desc = []
+    news = []
+    img = []
+    pub = []
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        pub.append(f["publishedAt"])
+    mytech = zip(news , desc , img , pub)
+    return render(request , 'tech.html' , context={"mytech" : mytech})
