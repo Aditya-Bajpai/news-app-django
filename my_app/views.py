2320078c6b883a3958fe2d797359049037a11c4b
Aditya <aditya@Adityas-MacBook-Air.local>
2021-01-26 22:26:16 +0530

    added the source for thr full story

--- my_app/views.py
+++ my_app/views.py
@@ -9,28 +9,34 @@ def index(request):
     desc =[] 
     news =[] 
     img =[] 
+    pub = []
+    url = []
 
     for i in range(len(l)): 
         f = l[i] 
         news.append(f['title']) 
         desc.append(f['description']) 
         img.append(f['urlToImage']) 
-    mylist = zip(news, desc, img) 
+        pub.append(f['publishedAt'])
+        url.append(f['url'])
+    mylist = zip(news, desc, img , pub , url) 
     return render(request, 'index.html', context ={"mylist":mylist})
 
 def technews(request):
     newsapi = NewsApiClient(api_key = "a0595ccff26a4790a9dd4b8c1e7e7c01")
-    top_headlines = newsapi.get_top_headlines(sources = "techcrunch")
+    top_headlines = newsapi.get_top_headlines(sources = "the-next-web")
     l = top_headlines["articles"]
     desc = []
     news = []
     img = []
     pub = []
+    url = []
     for i in range(len(l)):
         f = l[i]
         news.append(f['title'])
         desc.append(f['description'])
         img.append(f['urlToImage'])
         pub.append(f["publishedAt"])
-    mytech = zip(news , desc , img , pub)
+        url.append(f['url'])
+    mytech = zip(news , desc , img , pub , url)
     return render(request , 'tech.html' , context={"mytech" : mytech})
